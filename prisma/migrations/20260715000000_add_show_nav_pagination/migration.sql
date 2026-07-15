18be98dc3bae1deed3b0228010ad2e21b38f0ec8
Safa Tash <safatash@Safas-MBP.fios-router.home>
2026-07-15 11:05:32 -0400

    revert: remove migration causing P1002 error

--- prisma/migrations/20260715000000_add_show_nav_pagination/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddColumn showNav
-ALTER TABLE "ReviewWidget" ADD COLUMN "showNav" BOOLEAN NOT NULL DEFAULT true;
-
--- AddColumn showPagination  
-ALTER TABLE "ReviewWidget" ADD COLUMN "showPagination" BOOLEAN NOT NULL DEFAULT true;
